7e453ae760f4ceafc9df9af1eab38d6bd3119e63
seongchankim1 <167050690+seongchankim1@users.noreply.github.com>
2024-05-03 16:47:45 +0900

    #5 수정 기능 완성!

--- src/model/Score.java
+++ src/model/Score.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Score {
     private String scoreId;
     private String subjectId;
@@ -14,5 +18,37 @@ public class Score {
         this.studentId = studentId;
         this.round = round;
         this.score = score;
+    }
+
+
+    // score 점수를 반환
+    public int getScore() {
+        return score;
+    }
+
+public int setScore(int score) {
+        this.score = score;
+        return score;
+}
+public String getSubjectId() {
+        return subjectId;
+}
+    public int getRound() {
+        return round;
+    }
+
+    public boolean matches(String studentId, String subjectId, int round) {
+        return this.studentId.equals(studentId) && this.subjectId.equals(subjectId) && this.round == round;
+    }
+    public static HashMap<String, List<Score>> scoreMap = new HashMap<>();
+
+    private static Score findScore(String studentId, String subjectId, int round) {
+        List<Score> scores = scoreMap.getOrDefault(studentId, new ArrayList<>());
+        for (Score score : scores) {
+            if (score.matches(studentId, subjectId, round)) {
+                return score;
+            }
+        }
+        return null; // 점수 정보가 없을 경우 null 반환
     }
 }
