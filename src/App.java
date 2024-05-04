29c769aa5906e3f2eff5a24d67c55a1d6c6fa3ab
gaeun7 <rkdms1111111@naver.com>
2024-05-04 15:58:51 +0900

    1차 수정 중,,

--- src/App.java
+++ src/App.java
@@ -1,3 +1,8 @@
+/*import model.Score;
+import model.Student;
+import model.Subject;
+import model.SubjectType;*/
+
 import model.*;
 
 import java.util.*;
@@ -117,9 +122,8 @@ public class App {
                 case 1 -> displayStudentView(); // 수강생 관리
                 case 2 -> displayScoreView(); // 점수 관리
                 case 3 -> flag = false; // 프로그램 종료
-                default -> {
-                    System.out.println("잘못된 입력입니다.\n되돌아갑니다!");
-                }
+                default -> System.out.println("잘못된 입력입니다.\n되돌아갑니다!");
+
             }
         }
         System.out.println("프로그램을 종료합니다.");
@@ -139,7 +143,6 @@ public class App {
             int input = sc.nextInt();
 
             switch (input) {
-                case 0 -> flag = false;
                 case 1 -> createStudent();
                 case 2 -> studentInquiry();
                 case 3 -> modifyStudentInfo();
@@ -180,6 +183,39 @@ public class App {
                     System.out.println("잘못된 입력입니다.");
                 }
             }
+        }
+        // 전체 회차 성적 조회
+        System.out.print("학생의 ID를 입력하세요: ");
+        String studentId = sc.next();
+
+        System.out.print("과목을 입력하세요 (Java, 객체지향, Spring, JPA, MySQL, 디자인 패턴, Spring Security, Redis, MongoDB): ");
+        String subjectName = sc.next();
+
+        if (studentList.containsKey(studentId)) {
+            Student student = studentList.get(studentId);
+            Map<Subject, Map<Integer, Score>> scores = student.getScores();
+            boolean foundSubject = false;
+
+            for (Map.Entry<Subject, Map<Integer, Score>> entry : scores.entrySet()) {
+                Subject subject = entry.getKey();
+                if (subject.getSubjectName().equals(subjectName)) {
+                    foundSubject = true;
+                    Map<Integer, Score> scoreMap = entry.getValue();
+                    System.out.println("[" + subjectName + "] 성적 조회");
+                    for (Map.Entry<Integer, Score> scoreEntry : scoreMap.entrySet()) {
+                        int round = scoreEntry.getKey();
+                        Score score = scoreEntry.getValue();
+                        System.out.println("회차: " + round + ", 성적: " + score.getGrade());
+                    }
+                    break;
+                }
+            }
+
+            if (!foundSubject) {
+                System.out.println("해당 과목의 성적이 없습니다.");
+            }
+        } else {
+            System.out.println("학생을 찾을 수 없습니다.");
         }
     }
 
